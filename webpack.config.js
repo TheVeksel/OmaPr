53371f61adfd15673b4a95a877ab7908ae8796d1
TheVeksel <masxlesfrom@gmail.com>
2024-11-03 21:35:21 +0300

    vercel_fix

--- webpack.config.js
+++ webpack.config.js
@@ -6,55 +6,55 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/', // Устанавливаем publicPath для поддержки маршрутизации
+    path: path.resolve(__dirname, 'dist'), // Путь к выходной директории
+    filename: 'bundle.js', // Имя выходного файла
+    publicPath: '/', // Путь для доступа к статическим файлам
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.jsx'],
+    extensions: ['.js', '.ts', '.tsx', '.jsx'], // Расширения файлов
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.tsx?$/, // Для TypeScript файлов
         use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/,
+        test: /\.css$/, // Для CSS файлов
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.scss$/,
+        test: /\.scss$/, // Для SCSS файлов
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Для изображений
         type: 'asset/resource',
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, // Для шрифтов
         type: 'asset/resource',
       },
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(), // Очищает выходную папку перед каждой сборкой
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: './public/index.html', // Шаблон для создания index.html
     }),
-    new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'public/photo', to: 'photo' },  
+        { from: 'public/photo', to: 'photo' }, // Копирование папки с изображениями
       ],
     }),
   ],
   devServer: {
-    static: './dist',
-    port: 3000,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
+    static: path.resolve(__dirname, 'dist'), // Путь к статическим файлам для devServer
+    port: 3000, // Порт для сервера
+    open: true, // Автоматически открывать браузер
+    hot: true, // Включить горячую замену модулей
+    historyApiFallback: true, // Для поддержки маршрутизации
   },
-  mode: 'development',
+  mode: 'development', // Режим разработки
 };
